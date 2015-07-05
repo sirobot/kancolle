903efe44ac80ff9f45e6b5dd222893f6fd9d222a
siro <opfer@dunkel.halfmoon.jp>
2015-07-05 19:28:47 +0900

    刀剣男士所有一覧側で使用しているソースコードを艦娘所有一覧に合わせて修正

--- index.php
+++ index.php
@@ -1,120 +1,161 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="canonical" href="http://dunkel.halfmoon.jp/kancolle/"" />
+        <title>艦娘所有一覧</title>
+		<link rel="stylesheet" href="themes/kan_jquery.min.css" />
+		<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+		<link href="kan_style.css" rel="stylesheet" type="text/css">
+		<link href="image-picker/image-picker.css" rel="stylesheet" type="text/css">
+		<script src="image-picker/image-picker.min.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				// 刀剣男士所有一覧のソースをベースに全面改修
+				// 既存互換は廃止
 
-<head>
-<meta content="ja" http-equiv="Content-Language">
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="canonical" href="http://dunkel.halfmoon.jp/kancolle/" />
-<title>艦娘所有一覧</title>
+				// 入力(POST)読み込み
+				var presetList = $('#presetList').val();
+				if (presetList !== '') {
+					var presetListArray = presetList.split(',');
+					$('#kancolleselect option').filter(function() {
+						return jQuery.inArray($(this).val(), presetListArray) != -1;
+					}).attr('selected', 'selected');
+				}
+				// imagePicker初期化
+				$(".kancolle").imagepicker({hide_select: true, show_label: true});
+				// 全選択
+				$('#chkAll,#chkAll_b').click(function() {
+					$('#kancolleselect option').each(function() {
+						$(this).attr('selected', 'selected');
+					});
+					$('ul.thumbnails li div.thumbnail').addClass('selected');
+				});
+				// 全選択解除
+				$('#chkReset,#chkReset_b').click(function() {
+					$('#kancolleselect option').each(function() {
+						$(this).removeAttr('selected');
+					});
+					$('ul.thumbnails li div.thumbnail').removeClass('selected');
+				});
+				// フォーム送信データ作成
+				$("form").submit(function() {
+					var formValArray = $('#form1 #kancolleselect').val();
+					// ['1','7',...]の配列になっている状態のものを01に変換
+					// twitterのフォロワーの皆様実装アイディアありがとうございます
+					var formVal = [];
+					// formVal = {'0','0','0','0','0','0',...'0'}の配列を生成・初期化
+					for (var i = 1; i < formValArray[formValArray.length - 1]; i++) {
+						formVal[i] = '0';
+					}
+					// formValArrayの要素を添字に、必要な部分のみ1に上書き
+					for (var i = 0; i < formValArray.length; i++) {
+						formVal[formValArray[i]] = '1';
+					}
+					// (ver1.1)5文字ずつ32進数で圧縮
+					// 末尾が5文字に満たない場合は0詰め
+					console.log('formaVal.length:' + (formVal.length-1));
+					for (var i = 0; i < (formVal.length - 1) % 5; i++){
+						formVal.push('0');
+					}
+					var form32Val = '';
+					var tempStr = '';
+					var tempInt = 0;
+					console.log('formaVal.length:' + (formVal.length-1));
+					for (var i = 1;	i < formVal.length; i=i+5){
+						tempStr = formVal[i] + formVal[i+1] +formVal[i+2] + formVal[i+3] + formVal[i+4];
+						console.log('tempStr:' + tempStr);
+						tempInt = parseInt(tempStr,2);
+						console.log('tempInt:' + tempInt);
+						form32Val = form32Val + tempInt.toString(32);
+						console.log('form32Val:' + form32Val);
+					}
+					formVal = formVal.join('');
+					$('#form1 #kancolleselect').val(null);
+					$('#form1 #kancolleList').val(form32Val);
+					console.log('test::');
+					console.log(form32Val);
+					console.log('::endTest');
+				});
+			});
+		</script>
 
-<link rel="stylesheet" type="text/css" href="flat-ui/jquery.mobile.flatui.css" />
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+    </head>
+    <body>
+		<input type="hidden" id="presetList" name="presetList" value="<?php echo($_POST['presetList']); ?>">
+		<div data-role="page">
+			<div data-role="header" data-theme="f">
+				<h1>艦娘所有一覧</h1>
+			</div>
+			<div data-role="content">
+				<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="info" data-collapsed="false"   data-theme="f">
+					<h3>これなに？</h3> 
+					<p class="collapsible">艦隊これくしょん～艦これ～の艦娘所有一覧ジェネレーターです。</p>
+					<p class="collapsible">持っている艦娘をクリックして選択し、「作成！」ボタンを押すと作成できます。</p>
+					<p class="collapsible">最近のブラウザならだいたい動くと思います。</p>
+					<br />
+					<a href="https://twitter.com/share" class="twitter-share-button" data-lang="ja">ツイート</a>
+					<script>!function(d, s, id) {
+							var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+							if (!d.getElementById(id)) {
+								js = d.createElement(s);
+								js.id = id;
+								js.src = p + '://platform.twitter.com/widgets.js';
+								fjs.parentNode.insertBefore(js, fjs);
+							}
+						}(document, 'script', 'twitter-wjs');
+					</script>
+				</div>
+				<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="grid" data-collapsed="false"  data-theme="f">
+					<h3>艦娘一覧</h3>
+					<form action="result.php" method="get" data-ajax="false" name="form1" id="form1">
+						<input type="hidden" id="kancolleList" name="kancolleList" value="">
+						<input type="hidden" id="ver" name="ver" value="1.1">
+						<button id="chkAll" data-icon="check" data-theme="f" data-inline="true" type="button">すべて選択</button>
+						<button id="chkReset" data-icon="delete" data-theme="f" data-inline="true" type="reset">選択リセット</button>
+						<input data-theme="c" data-inline="true" type="submit" value="作成">
+						<?php
+						$path = "/home/vage/pear/PEAR/";
+						set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+						require_once "../../siro_common/php/db_common.php";
 
-<link href="kan_style.css" rel="stylesheet" type="text/css">
-<link href="image-picker/image-picker.css" rel="stylesheet" type="text/css">
-<script src="image-picker/image-picker.min.js" type="text/javascript"></script>
-<script src="plugin/jquery.cookie.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(document).ready(function(){
-	// クッキー読み込み
-	var musuListCookie = $.cookie('musuListCookie');
-	if (musuListCookie){
-		var musuListArray = musuListCookie.split(',');
-		$('#mususelect option').filter(function(){
-			return jQuery.inArray($(this).val(),musuListArray) != -1;
-		})
-		.attr('selected', 'selected');
-	}
-	// imagePicker初期化
-	$(".musu").imagepicker({hide_select: true, show_label: true});
-	// 全選択
-	$('#chkAll,#chkAll_b').click(function(){
-		$('#mususelect option').each(function() {
-			$(this).attr('selected', 'selected');
-		});
-		$('ul.thumbnails li div.thumbnail').addClass('selected');
-	});
-	// 全選択解除
-	$('#chkReset,#chkReset_b').click(function(){
-		$('#mususelect option').each(function() {
-			$(this).removeAttr('selected');
-		});
-		$('ul.thumbnails li div.thumbnail').removeClass('selected');
-	});
-	// フォーム送信データ作成
-	$("form").submit(function(){
-		var formVal = $('#form1 #mususelect').val();
-		$('#form1 #mususelect').val(null);
-		$('#form1 #musuList').val(formVal);
-		$.cookie('musuListCookie', formVal, { expires: 14 , path: '/'});
-	});
-});
-</script>
-</head>
+						// DBから艦娘のデータを取得
+						$db_conn = db_conn();
+						if ($db_conn !== false) {
+							$query = mysql_query("select * from kancolle_charaList where shipType is not null order by number");
+							// selectタグの出力
+							echo '<select id="kancolleselect" multiple="multiple" class="kancolle" data-role="none" name="m[]">';
+							while ($kancolle = mysql_fetch_object($query)) {
+								echo "\t<option data-img-src='image/" . $kancolle->number . ".jpg' value='" . ltrim($kancolle->number,"0") . "'>No." . $kancolle->number . " " . $kancolle->name . "</option>\n";
+							}
+							echo '</select>';
+							mysql_close($db_conn);
+						}
+						?>
+						<button id="chkAll_b" data-icon="check" data-theme="f" data-inline="true" type="button">すべて選択</button>
+						<button id="chkReset_b" data-icon="delete" data-theme="f" data-inline="true" type="reset">選択リセット</button>
+						<input data-theme="c" data-inline="true" type="submit" value="作成">
+					</form>
+				</div>
+				<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="mail" data-collapsed="true"  data-theme="f">
+					<h3>他</h3>
+					<p class="collapsible">何か不具合等ありましたらご連絡ください。</p>
+					<p class="collapsible">作成：siro Twitter:@siro_xx</p>
+				</div>
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- kancolle -->
+				<ins class="adsbygoogle"
+				     style="display:inline-block;width:728px;height:90px"
+				     data-ad-client="ca-pub-1725571372992163"
+				     data-ad-slot="2774351136"></ins>
+				<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
 
-<body>
-<div data-role="page">
-<div data-role="header" data-theme="f">
-<h1>艦娘所有一覧</h1>
-</div>
-<div data-role="content">
-<div data-role="collapsible" data-collapsed-icon="flat-menu" data-expanded-icon="flat-cross" data-collapsed="false"  data-theme="f">
-	<h3>これなに？</h3> 
-	<p class="collapsible">艦隊これくしょん～艦これ～の艦娘所有一覧ジェネレーターです。</p>
-	<p class="collapsible">持っている艦娘をクリックして選択し、「作成！」ボタンを押すと作成できます。</p>
-	<p class="collapsible">最近のブラウザならだいたい動くと思います。</p>
-	<br />
-	<a href="https://twitter.com/share" class="twitter-share-button" data-lang="ja">ツイート</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
-</div>
-<div data-role="collapsible" data-collapsed-icon="flat-checkround" data-expanded-icon="flat-cross" data-collapsed="false"  data-theme="f">
-<h3>艦娘一覧</h3>
-<form action="result.php" method="get" data-ajax="false" name="form1" id="form1">
-<input type="hidden" id="musuList" name="musuList" value="">
-<button id="chkAll" data-icon="flat-checkround" data-theme="f" data-inline="true" type="button">すべて選択</button>
-<button id="chkReset" data-icon="flat-cross" data-theme="f" data-inline="true" type="reset">選択リセット</button>
-<input data-theme="d" data-inline="true" type="submit" value="作成！">
-<?php
-	$path = "/home/vage/pear/PEAR/";
-	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-	require_once "../../siro_common/php/db_common.php";
-	
-	// DBから艦娘のデータを取得
-	$db_conn = db_conn();
-	if($db_conn !== false){
-		$query = mysql_query("select * from kancolle_charaList order by number");
-		// selectタグの出力
-		echo '<select id="mususelect" multiple="multiple" class="musu" data-role="none" name="m[]">';
-		while($musu = mysql_fetch_object($query)){
-			echo "\t<option data-img-src='image/" . $musu->number . ".jpg' value='" . $musu->number . "'>No." . $musu->number . " " . $musu->shipType . " " . $musu->name . "</option>\n";
-		}
-		echo '</select>';
-		mysql_close($db_conn);
-	}
-?>
-<button id="chkAll_b" data-icon="flat-checkround" data-theme="f" data-inline="true" type="button">すべて選択</button>
-<button id="chkReset_b" data-icon="flat-cross" data-theme="f" data-inline="true" type="reset">選択リセット</button>
-<input data-theme="d" data-inline="true" type="submit" value="作成！">
-</form>
-</div>
-<div data-role="collapsible" data-collapsed-icon="flat-menu" data-expanded-icon="flat-cross" data-collapsed="true"  data-theme="f">
-	<h3>他</h3>
-	<p class="collapsible">何か不具合等ありましたらご連絡ください。</p>
-	<p class="collapsible">作成：siro Twitter:@siro_xx</p>
-</div>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- kancolle -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-1725571372992163"
-     data-ad-slot="2774351136"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-</div>
-</body>
-
+    </body>
 </html>
--- result.php
+++ result.php
@@ -1,109 +1,166 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-<meta content="ja" http-equiv="Content-Language">
-<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+	<head>
+		<meta content="ja" http-equiv="Content-Language">
+		<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>艦娘所有一覧</title>
+		<title>艦娘所有一覧</title>
 
-<link rel="stylesheet" type="text/css" href="flat-ui/jquery.mobile.flatui.css" />
-<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+		<link rel="stylesheet" href="themes/kan_jquery.min.css" />
+		<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+		<script type="text/javascript" src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 
-<link href="kan_style.css" rel="stylesheet" type="text/css">
-<link href="image-picker/image-picker.css" rel="stylesheet" type="text/css">
-<script src="image-picker/image-picker.min.js" type="text/javascript"></script>
-</head>
+		<link href="kan_style.css" rel="stylesheet" type="text/css">
+		<link href="image-picker/image-picker.css" rel="stylesheet" type="text/css">
+		<script src="image-picker/image-picker.min.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			$(document).ready(function() {
+				$('#flip_noncollect').click(function() {
+					if ($('#flip_noncollect').is(':checked')) {
+						// 未所持艦娘の表示
+						$(".noncollect").show();
+					} else {
+						// 未所持艦娘の非表示
+						$(".noncollect").hide();
+					}
+				});
+				$('#flip_unimpl').click(function() {
+					if ($('#flip_unimpl').is(':checked')) {
+						// 未実装艦娘の表示
+						$(".unimpl").show();
+					} else {
+						// 未実装艦娘の非表示
+						$(".unimpl").hide();
+					}
+				});
+			});
+		</script>
+	</head>
 
-<body>
-<div data-role="page">
-<div data-role="header" data-theme="f">
-<h1>艦娘所有一覧</h1>
-</div>
-<div data-role="content">
-<div data-role="collapsible" data-collapsed-icon="flat-checkround" data-expanded-icon="flat-cross" data-collapsed="false"  data-theme="f">
-<h3>メニュー</h3>
-<?php
-	$path = "/home/vage/pear/PEAR/";
-	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-	require_once "Services/ShortURL.php";
-	// 全No
-	define("MUSUNUM",240);
-	// 未実装数
-	define("MUSUMIJISSO",22);
-	// 未実装No
-	$mijissoMusumeList = array('162','198','199','200','203','217','218','220','222','223','224','227','228','230','232','233','234','235','237','238','239','240',);
-	// GETで回収
-	if(is_null($_GET['musuList'])){
-		// 既存互換
-		$form_musuList = $_GET['m'];
-	}else{
-		if($_GET['musuList'] == ""){
-			$form_musuList = null;
-		}else{
-			$form_musuList = explode(",",$_GET['musuList']);
-		}
-	}
-	// 未実装Noにチェックが入っている場合は削除
-	// 要素ゼロのときはチェックしない
-	if(is_array($form_musuList)){
-		$form_musuList = array_diff($form_musuList,$mijissoMusumeList);
-	}
-	// 所有率計算
-	$collectionRate = (count($form_musuList) / (MUSUNUM-MUSUMIJISSO)) * 100;
+	<body>
+		<div data-role="page">
+			<div data-role="header" data-theme="f">
+				<h1>艦娘所有一覧</h1>
+			</div>
+			<div data-role="content">
+				<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="info" data-collapsed="false"  data-theme="f">
+					<h3>メニュー</h3>
+					<?php
+					$path = "/home/vage/pear/PEAR/";
+					set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+					require_once "../../siro_common/php/db_common.php";
 
-	$originalUrl = "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-	$obj = Services_ShortURL::factory('TinyURL');
-	$shortUrl = $obj->shorten($originalUrl);
+					// GETで回収
+					if ($_GET['kancolleList'] == "") {
+						$form_kancolleList = array();
+						$selectedkancolleCount = 0;
+						$presetList = '';
+					} else {
+						// ver1.1(32進数圧縮)対応
+						if((float)$_GET['ver'] >= 1.1){
+							// 圧縮された文字列を01列に戻す
+							$temp_kancolleList32 = str_split($_GET['kancolleList']);
+							$temp_kancolleList = array();
+							foreach($temp_kancolleList32 as $base32){
+								$temp_str = str_pad(base_convert($base32, 32, 2),5,'0',STR_PAD_LEFT);
+								$temp_kancolleList = array_merge($temp_kancolleList,str_split($temp_str));
+							}
+						}else{
+							$temp_kancolleList = str_split($_GET['kancolleList']);							
+						}
+						$form_kancolleList = array();
+						foreach ($temp_kancolleList as $key => $val) {
+							if ($val === '1') {
+								$form_kancolleList[] = $key + 1;  // 添字を1からにする
+							}
+						}
+						$selectedkancolleCount = count($form_kancolleList);
+						$presetList = implode(',', $form_kancolleList);
+						$form_kancolleList[] = '0'; // ダミーデータを先頭に挿入し、添字を1からにする
+					}
+					// DBから艦娘のデータを取得
+					$db_conn = db_conn();
+					if ($db_conn !== false) {
+						$query = mysql_query("select * from kancolle_charaList where shipType is not null order by number");
+						$impl_kancolleList = array();
+						while ($kancolle = mysql_fetch_object($query)) {
+							// 実装済み艦娘Noの配列作成
+							$impl_kancolleList[] = $kancolle->number;
+						}
+						$kancolleCount = count($impl_kancolleList);
+						mysql_close($db_conn);
+					}
 
-	echo '<a href="http://dunkel.halfmoon.jp/kancolle/" data-role="button" data-theme="f" data-inline="true" data-ajax="false">もう一度作る</a><br />';
-	echo '<div>';
-	echo '艦娘所有数 ' . count($form_musuList) . '/' . (MUSUNUM-MUSUMIJISSO) . ' (所有率' . floor($collectionRate) . '%)';
-	echo '</div>';
-	echo '<div class="menu">';
-	echo '<label for="textarea-a">共有用(コピー＆ペーストでご利用ください)：</label>';
-	echo '<textarea name="textarea" id="textarea-a">艦娘所有一覧を作成しました(所有率' . floor($collectionRate) . '%)：' . $shortUrl . '</textarea>';
-	echo '</div>';
-?>
-</div>
-<div data-role="collapsible" data-collapsed-icon="flat-checkround" data-expanded-icon="flat-cross" data-collapsed="false"  data-theme="f">
+					// 所有率計算
+					$collectionRate = floor(($selectedkancolleCount / $kancolleCount) * 100);
 
-<h3>艦娘所有一覧</h3>
-<div class="musuleResultList">
-<?php
-	// 整形
-	$musuList = array_fill(1,MUSUNUM,"noncollect");
-	// 要素ゼロのときはチェックしない
-	if(is_array($form_musuList)){
-		foreach($form_musuList as $musu){
-			$musuList[intval($musu)] = "";
-		}
-	}
-	// 出力
-	for($j=1;$j<=MUSUNUM;$j++){
-		echo '<div class="musumeResult ' . $musuList[$j] . '">';
-		echo '<img src="image/' . str_pad($j,3,"0",STR_PAD_LEFT) .  '.jpg" height="150" width="109"></div>';
-	}
-/*
-<div class="musumeResult"><img alt="戦艦 長門" height="150" src="image/001.jpg" width="109"></div>
-<div class="musumeResult noncollect"><img alt="戦艦 陸奥" height="150" src="image/002.jpg" width="109"></div>
-*/
-?>
-</div>
-</div>
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- kancolle_result -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:15px"
-     data-ad-client="ca-pub-1725571372992163"
-     data-ad-slot="2634750330"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-</div>
-</div>
-</body>
+					$originalUrl = "http://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+					//$obj = Services_ShortURL::factory('TinyURL');
+					//$shortUrl = $obj->shorten($originalUrl);
+					//echo '<a href="http://dunkel.halfmoon.jp/kancolle/" data-role="button" data-theme="f" data-inline="true" data-ajax="false">この結果を編集する</a><br />';
+
+					echo '<form action="index.php" method="post" data-ajax="false" name="form1" id="form1">';
+					echo '<input type="hidden" id="preset" name="presetList" value="' . $presetList . '">';
+					echo '<input data-theme="f" data-inline="true" type="submit" value="この結果を編集する"></form>';
+
+					echo '<div>';
+					echo '艦娘所有数 ' . $selectedkancolleCount . '/' . $kancolleCount . ' (所有率' . $collectionRate . '%)';
+					echo '</div>';
+					echo '<div class="menu">';
+					echo '<a href="https://twitter.com/share" class="twitter-share-button" data-text="';
+					echo '艦娘所有一覧を作成しました(所有率' . $collectionRate . '%)';
+					echo '" data-count="none">Tweet</a>';
+					echo "<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
+					echo '</div>';
+					?>
+				</div>
+				<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="grid" data-collapsed="false"  data-theme="f">
+
+					<h3>艦娘所有一覧</h3>
+					<div class="kancolleleResultList">
+						<fieldset data-role="controlgroup" data-type="horizontal">
+							<legend>表示切替：</legend>
+							<input id="flip_noncollect" name="flip_show" type="checkbox" value="noncollect" />
+							<label for="flip_noncollect">未所持艦娘を表示</label>
+							<input id="flip_unimpl" name="flip_show" type="checkbox" value="unimpl" />
+							<label for="flip_unimpl">未実装艦娘を表示</label>
+						</fieldset>
+						<?php
+						// 出力
+						for ($j = 1; $j <= max($impl_kancolleList); $j++) {
+							// 「未実装」「実装済未所持」「所持」で表示を切り替える
+							if (in_array($j, $form_kancolleList)) {
+								// 所持
+								echo '<div class="musumeResult">';
+								echo '<img src="image/' . sprintf('%03d', $j) . '.jpg" height="150" width="109"></div>';
+							} elseif (in_array($j, $impl_kancolleList)) {
+								// 実装済未所持
+								echo '<div class="musumeResult noncollect" style="display: none">';
+								echo '<img src="image/' . sprintf('%03d', $j) . '.jpg" height="150" width="109"></div>';
+							} else {
+								// 未実装
+								echo '<div class="musumeResult unimpl" style="display: none">';
+								echo '<img src="image/000.jpg" height="150" width="109"></div>';
+							}
+						}
+						?>
+					</div>
+				</div>
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- kancolle_result -->
+				<ins class="adsbygoogle"
+				     style="display:inline-block;width:728px;height:15px"
+				     data-ad-client="ca-pub-1725571372992163"
+				     data-ad-slot="2634750330"></ins>
+				<script>
+				(adsbygoogle = window.adsbygoogle || []).push({});
+				</script>
+			</div>
+		</div>
+	</body>
 
 </html>
